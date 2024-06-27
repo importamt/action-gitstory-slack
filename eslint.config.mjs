5031fa47b48a460db0bb99c73f4539df14ef2d8b
임보탬 <bt.im@cj.net>
2024-06-27 16:05:26 +0900

    env: Add ESLint and Prettier config files, and update package-lock.json
    The commit includes ESLint and Prettier configuration files for setting up linting and formatting rules for the project. The package-lock.json file is also updated to record the specific version of each dependency for deterministic installations across machines.

---
+++ eslint.config.mjs
@@ -0,0 +1,8 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+]
